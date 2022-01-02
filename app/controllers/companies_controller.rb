096d04ebc1512ba77823b9bcdeb5fb753fdd1ea5
flaviomahoney <flavio@mahoneyt.com>
2022-01-01 22:21:15 -0300

    Created full company crud

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -8,6 +8,38 @@ class CompaniesController < ApplicationController
     def show
     end
 
+    def new
+        @company = Company.new
+    end
+    
+    def edit
+    end
+    
+      def create
+        @company = Company.new(company_params)
+        @company.user = current_user
+        if @company.save
+          flash[:success] = "Object successfully created"
+          redirect_to @company
+        else
+          flash[:error] = "Something went wrong"
+          render 'new'
+        end
+      end
+    
+      def update
+        if @company.update(company_params)
+          redirect_to @company, notice: 'company was successfully updated.'
+        else
+          render :edit
+        end
+      end
+    
+      def destroy
+        @company.destroy
+        redirect_to companys_url, notice: 'company was successfully destroyed.'
+      end
+
     private
 
     def set_pet
