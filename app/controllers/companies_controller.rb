c6de1ed00c4cda77afefee5a9cb49580ae8d1385
flaviomahoney <flavio@mahoneyt.com>
2022-01-01 22:09:35 -0300

    Added show configuration to company controller

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,5 +1,21 @@
 class CompaniesController < ApplicationController
+    before_action :set_pet, only: [:show, :edit, :update, :destroy]
+    
     def index
         @companies = Company.all
     end
+
+    def show
+    end
+
+    private
+
+    def set_pet
+      @company = Company.find(params[:id])
+    end
+  
+    def pet_params
+      params.require(:company).permit(:name, :contact, :photo_url)
+    end
+
 end
